4630386defc01930622e1062e647fc0e4f7a64b3
drwyspa <p.jedryczkowski@gmail.com>
2025-08-27 21:52:16 +0200

    Commit from 27.08

--- cpp00/ex01/src/PhoneBook.cpp
+++ cpp00/ex01/src/PhoneBook.cpp
@@ -1,12 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pjedrycz <p.jedryczkowski@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 20:13:30 by pjedrycz          #+#    #+#             */
-/*   Updated: 2025/08/18 20:13:31 by pjedrycz         ###   ########.fr       */
+/*   Updated: 2025/08/27 21:50:27 by pjedrycz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/PhoneBook.hpp"
+
+PhoneBook::PhoneBook()
+{
+	nb_Contact = 0;
+}
+
+void	chk_PhoneBook()
+{
+	if (PhoneBook::nb_Contact == 8)
+	{
+		
+	}
+}
+
+int	PhoneBook::addContact()
+{
+
+}
