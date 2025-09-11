49685cea612bb4ecccde154c57beae734334935e
drwyspa <p.jedryczkowski@gmail.com>
2025-09-11 21:39:21 +0200

    update

--- cpp01/ex01/src/Zombie.cpp
+++ cpp01/ex01/src/Zombie.cpp
@@ -6,9 +6,27 @@
 /*   By: pjedrycz <p.jedryczkowski@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 20:49:18 by pjedrycz          #+#    #+#             */
-/*   Updated: 2025/09/11 21:00:27 by pjedrycz         ###   ########.fr       */
+/*   Updated: 2025/09/11 21:34:06 by pjedrycz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Zombie.hpp"
 
+Zombie::Zombie() : name(""){}
+
+Zombie::Zombie(std::string name) : name(name) {}
+
+void	Zombie::setName(std::string name)
+{
+	this->name = name;
+}
+
+void	Zombie::announce(void) const
+{
+	std::cout << name << ": Moooaaarrrr BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << "Zombie named: " << name << " has been destroyed!" << std::endl;
+}
