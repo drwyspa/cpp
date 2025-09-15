af2a2acb952d62ae7970e83008d6f93a9d156543
drwyspa <p.jedryczkowski@gmail.com>
2025-09-15 21:11:11 +0200

    Update

--- cpp01/ex01/src/main.cpp
+++ cpp01/ex01/src/main.cpp
@@ -6,9 +6,23 @@
 /*   By: pjedrycz <p.jedryczkowski@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 21:00:31 by pjedrycz          #+#    #+#             */
-/*   Updated: 2025/09/11 21:00:34 by pjedrycz         ###   ########.fr       */
+/*   Updated: 2025/09/15 21:09:02 by pjedrycz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Zombie.hpp"
 
+Zombie*	zombieHorde(int N, std::string name);
+
+int	main(void)
+{
+	int	N = 3;
+	Zombie*	zSoldier = zombieHorde(N, "Soldier_");
+
+	for (int i = 0; i < N; i++)
+		zSoldier[N].announce();
+
+	delete[] zSoldier;
+
+	return 0;
+}
--- cpp01/ex01/src/zombieHorde.cpp
+++ cpp01/ex01/src/zombieHorde.cpp
@@ -6,16 +6,24 @@
 /*   By: pjedrycz <p.jedryczkowski@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/11 20:49:45 by pjedrycz          #+#    #+#             */
-/*   Updated: 2025/09/11 21:25:54 by pjedrycz         ###   ########.fr       */
+/*   Updated: 2025/09/15 21:04:50 by pjedrycz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Zombie.hpp"
 
-
+//Create N number of Zombies. 
+//Give each a "name".
 
 
 Zombie*	zombieHorde(int N, std::string name)
 {
-	
+	Zombie*	zSoldier = new Zombie[N];
+	for (int i = 0; i < N; i++)
+	{
+		std::ostringstream oss;
+		oss << name << (i + 1);
+		zSoldier[i].setName(oss.str());
+	}
+	return zSoldier;
 }
