6e8cea23adb6768842d02161b5870187e879eb99
Przemysław Jędryczkowski <86292163+drwyspa@users.noreply.github.com>
2025-10-29 19:13:47 +0100

    Add files via upload

---
+++ cpp04/ex00/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pjedrycz <p.jedryczkowski@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/29 18:47:25 by pjedrycz          #+#    #+#             */
+/*   Updated: 2025/10/29 18:52:24 by pjedrycz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main()
+{
+const Animal* meta = new Animal();
+const Animal* j = new Dog();
+const Animal* i = new Cat();
+std::cout << j->getType() << " " << std::endl;
+std::cout << i->getType() << " " << std::endl;
+i->makeSound(); //will output the cat sound!
+j->makeSound();
+meta->makeSound();
+...
+return 0;
+}
