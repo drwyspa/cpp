87bc7f5994f13d32f230f6b369f79cf8ba3d2773
drwyspa <p.jedryczkowski@gmail.com>
2025-12-03 11:13:20 +0100

    CPP05 - ex00 - init commit

---
+++ cpp05/ex00/inc/Bureaucrat.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pjedrycz <p.jedryczkowski@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 21:43:24 by pjedrycz          #+#    #+#             */
+/*   Updated: 2025/12/02 21:46:36 by pjedrycz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <string>
+
+class Bureaucrat
+{
+
+};
+
+#endif
