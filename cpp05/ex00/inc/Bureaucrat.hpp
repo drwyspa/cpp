1b4c111b2759d83ac58e19890b49302c39564084
drwyspa <p.jedryczkowski@gmail.com>
2025-12-03 22:18:03 +0100

    update

--- cpp05/ex00/inc/Bureaucrat.hpp
+++ cpp05/ex00/inc/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: pjedrycz <p.jedryczkowski@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 21:43:24 by pjedrycz          #+#    #+#             */
-/*   Updated: 2025/12/02 21:46:36 by pjedrycz         ###   ########.fr       */
+/*   Updated: 2025/12/03 22:04:56 by pjedrycz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,43 @@
 
 # include <iostream>
 # include <string>
+# include <exception>
 
 class Bureaucrat
 {
+	private:
+		const std::string	name;
+		int					grade;
 
+	public:
+		Bureaucrat (const std::string &name, int grade);
+		Bureaucrat (const Bureaucrat &other);
+		Bureaucrat &operator=(const Bureaucrat &other);
+		~Bureaucrat();
+
+		//Getters//
+		const std::string	&getName() const;
+		int					getGrade() const;
+
+		//Grade change methods//
+		void	incrementGrade();
+		void	decrementGrade();
+
+		//Exceptions//
+		class	GradeTooHighExceptio : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+
+		class	GradeTooLowException : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
 };
+
+//Operator <<//
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
 
 #endif
