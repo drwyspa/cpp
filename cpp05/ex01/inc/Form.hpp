a6fc5bebff96e801377e135af470cda7c1963d6d
drwyspa <p.jedryczkowski@gmail.com>
2025-12-29 22:37:34 +0100

    Adding form.hpp

--- cpp05/ex01/inc/Form.hpp
+++ cpp05/ex01/inc/Form.hpp
@@ -6,7 +6,48 @@
 /*   By: pjedrycz <p.jedryczkowski@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 21:03:21 by pjedrycz          #+#    #+#             */
-/*   Updated: 2025/12/10 21:03:22 by pjedrycz         ###   ########.fr       */
+/*   Updated: 2025/12/29 22:37:27 by pjedrycz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+class	Bureaucrat;
+
+class	Form
+{
+	private:
+		const std::string	name;
+		bool				isSigned;
+		const int			signGrade;
+		const int			execGrade;
+
+	public:
+		Form(const std::string &name, int signGrade, int execGrade);
+		Form(const Form &other);
+		Form &operator=(const Form &other);
+		~Form();
+
+		void	beSigned(const Bureaucrat &bureaucrat);
+
+		class	GradeTooHighException : public std::exception
+		{
+			public:
+				const char*	what() const throw();
+		};
+
+		class	GradeTooLowException : public std::exception
+		{
+			public:
+				const char*	what() const throw();
+		};
+};
+
+std::ostream	&operator<<(std::ostream &os, const Form &form);
+
+#endif
